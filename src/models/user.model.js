5dd88cdb6884c3ead126836342971d1283c50cd4
Nitin594 <nitinmaurya594@gmail.com>
2025-08-08 08:27:08 +0530

    Added cloudinary and multer

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,36 +18,36 @@ const userSchema = new Schema (
         trim:true
     },
     fullname: {
-    type:String,
-    required:true,
-    unique:true,
-    lowercase:true,
-    trim:true,
-    index:trusted
-},
-avatar:{
-    type:String, //cloudinary url
-    required:true,
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:trusted
+    },
+    avatar:{
+        type:String, //cloudinary url
+        required:true,
 
-},
-coverImage:{
-    type:String, //cloudinary url
-},
-watchHistory:[
+    },
+    coverImage:{
+        type:String, //cloudinary url
+    },
+    watchHistory:[
     {
         type:Schema.Types.ObjectId,
         ref:"Video"
     }
-],
-password:{
-    type:String,
-    required:[true,'Password is required']
-},
-refreshToken:{
-    type:String
-}
+    ],
+    password:{
+        type:String,
+        required:[true,'Password is required']
+    },
+    refreshToken:{
+        type:String
+    }
     
-},{timestamps:true}
+    },{timestamps:true}
 
 )
 userSchema.pre("save",async function (next){
