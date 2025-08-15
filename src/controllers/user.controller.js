1537b1433090c1611fbefe1ddc776d90fb166381
Nitin594 <nitinmaurya594@gmail.com>
2025-08-15 22:18:43 +0530

    DOne with login and logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.model.js"; //this user talks to database
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+     return { accessToken, refreshToken }; 
+  } catch (error) {
+    throw new ApiError(
+      500,
+      " Something went wrong while generation access and refresh tokens"
+    );
+  }
+};
 //method to register user
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -39,8 +56,8 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "User with email or username already exists.");
   }
 
-  const avatarLocalPath = req.files?.avatar[0]?.path;   // If the user uploaded an avatar image, get the path of the first avatar file. If not, avatarLocalPath will be undefined.
-  let coverImageLocalPath;                              // .? is optional chaining
+  const avatarLocalPath = req.files?.avatar[0]?.path; // If the user uploaded an avatar image, get the path of the first avatar file. If not, avatarLocalPath will be undefined.
+  let coverImageLocalPath; // .? is optional chaining
   if (
     req.files &&
     Array.isArray(req.files.coverImage) &&
@@ -80,4 +97,84 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  //Todos
+  // req.body -> data
+  // username or email
+  // find the user
+  // password check
+  // access and refresh token
+  // send cookies
+  // send res
+
+  const { email, username, password } = req.body;
+
+  if (!(username || email)) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (!user) {
+    throw new ApiError(400, "User is not found!");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully!"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"));
+});
+
+export { registerUser, loginUser, logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser,logoutUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
@@ -11,6 +12,10 @@ router.post("/register",
   { name: "coverImage", maxCount: 1 }
 ]),
     registerUser);
+
+router.route("/login").post(loginUser)
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser)
 // filepath: d:\myYoutube\src\routes\user.routes.js
 router.get("/test", (req, res) => res.send("Test route working"));
 //router.route("/login").post(login)
